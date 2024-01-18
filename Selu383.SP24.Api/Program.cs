2601b4f0b9c375af37c80d89d8de03ba82a28094
Sheylo1 <wesley.austin-2@selu.edu>
2024-01-18 10:32:49 -0600

    test commit

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+// commit test
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
