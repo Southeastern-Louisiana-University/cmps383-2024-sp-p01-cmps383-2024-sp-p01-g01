2407d8ec2748fba2eb55050f6d0f9adb3cc38bd2
Sheylo1 <wesley.austin-2@selu.edu>
2024-01-23 13:16:17 -0600

    Hotel-Hardcoded-Get-Controller
    Hardcoded Hotel HTTPGET with the hotel entity

---
+++ Selu383.SP24.Api/Controllers/HotelController.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Selu383.SP24.Api.Entities;
+
+namespace Selu383.SP24.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelController : ControllerBase
+    {
+
+        private readonly ILogger<HotelController> _logger;
+
+        public HotelController(ILogger<HotelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var entity = new Hotel
+            {
+                Id = 1,
+                Name = "Hotel BnB",
+                Address = "1337 Hammond Drive"
+            };
+
+            return Ok(entity);
+        }
+    }
+ 
+}
+
