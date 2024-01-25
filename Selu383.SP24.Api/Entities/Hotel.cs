783589aaf7b9bf14dafce7463ff06ad77daf9ba2
Sheylo1 <wesley.austin-2@selu.edu>
2024-01-24 18:13:33 -0600

    Declared DataContext

--- Selu383.SP24.Api/Entities/Hotel.cs
+++ Selu383.SP24.Api/Entities/Hotel.cs
@@ -8,6 +8,13 @@ namespace Selu383.SP24.Api.Entities
         public string? Address { get; set; }
     }
 
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+
     public class HotelGetDto
     {
         public int Id { get; set; }
