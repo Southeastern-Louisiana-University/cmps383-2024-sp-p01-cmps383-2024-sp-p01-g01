7f35ce0f86fcc9217d13b57c1a7634408b7fdbf8
Kade Lane <lanekade8@gmail.com>
2024-01-25 16:27:23 -0600

    "Post" and "GetAll" for HotelController. Throwing an error when calling the DataContext.

--- Selu383.SP24.Api/Controllers/HotelController.cs
+++ Selu383.SP24.Api/Controllers/HotelController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Selu383.SP24.Api.Entities;
+using Selu383.SP24.Api.Controllers;
 
 namespace Selu383.SP24.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/hotels")]
     public class HotelController : ControllerBase
     {
         private readonly ILogger<HotelController> _logger;
@@ -26,5 +27,32 @@ namespace Selu383.SP24.Api.Controllers
 
             return Ok(Hotels);
         }
+
+
+        /*
+        [HttpPost]
+        public ActionResult MakeHotel(HotelDto hotel)
+        {
+            DataContext.Set<Hotel>().Add(new hotel);
+            DataContext.SaveChanges();
+
+            return Ok(hotel);
+        }
+        */
+
+        /*
+        [HttpGet("all")]
+        public List<HotelDto> GetAllHotels()
+        {
+            return DataContext.Set<HotelDto>()
+                .Select(x => new HotelDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Address = x.Address,
+                })
+                .ToList();
+        }
+        */
     }
 }
