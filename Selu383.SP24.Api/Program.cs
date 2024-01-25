3d478febc6ca78fc300fc00da2aca7818ce01487
Sheylo1 <wesley.austin-2@selu.edu>
2024-01-24 18:30:16 -0600

    Added DataContext
    PLEASE PLEASE PLEASE GET THE CONNECTION STRING TO THE DATABASE

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 //Testing testing
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
