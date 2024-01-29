5ea99aa797ed29a4f6ba2232eb2b984c7f8997d6
Sheylo1 <wesley.austin-2@selu.edu>
2024-01-29 13:05:32 -0600

    Fixed Migration Issue
    Used the trouble shooting document to fix the issue. Need to set up proper variable get/post/put/delete. Our local DB should be set up, may need to add queries

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -14,13 +14,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 // commit test
 
-/*
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
     db.Database.Migrate();
 }
-*/
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
