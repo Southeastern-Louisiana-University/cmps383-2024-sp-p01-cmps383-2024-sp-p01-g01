2e01a9c473dc03b3d64276b77784dd34bc7d4926
Kade Lane <lanekade8@gmail.com>
2024-01-30 13:05:43 -0600

    Brushing up Hotel entity

--- Selu383.SP24.Api/Entities/Hotel.cs
+++ Selu383.SP24.Api/Entities/Hotel.cs
@@ -14,29 +14,4 @@ namespace Selu383.SP24.Api.Entities
         public string? Name { get; set; }
         public string? Address { get; set; }
     }
-
-    public class HotelGetDto
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-    }
-
-    public class HotelCreateDto
-    {
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-    }
-
-    public class HotelUpdateDto
-    {
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-    }
-
-    public class HotelListingDto
-    {
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-    }
 }
